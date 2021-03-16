ec3cf14aa9dffb10bef024f6f76ca5e221f50218
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2021-03-15 17:19:57 -0700

    Improving all properties helpers

--- allProperties.go
+++ allProperties.go
@@ -1,27 +1,11 @@
 package vangogh_properties
 
-func AllProperties() []string {
+func AllTextProperties() []string {
 	return []string{
-		IdProperty, // must be first, to be removed when not needed
 		TitleProperty,
 		DeveloperProperty,
 		PublisherProperty,
-		ImageProperty,
-		BoxArtProperty,
-		BackgroundProperty,
-		GalaxyBackgroundProperty,
-		IconProperty,
-		LogoProperty,
-		ScreenshotsProperty,
 	}
-}
-
-func AllStashedProperties() []string {
-	// stashed properties
-	all := AllProperties()
-	// remove first property (IdProperty)
-	all[0] = all[len(all)-1]
-	return all[:len(all)-1]
 }
 
 func AllImageIdProperties() []string {
@@ -34,4 +18,14 @@ func AllImageIdProperties() []string {
 		LogoProperty,
 		ScreenshotsProperty,
 	}
+}
+
+func AllStashedProperties() []string {
+	all := AllTextProperties()
+	return append(all, AllImageIdProperties()...)
+}
+
+func AllProperties() []string {
+	all := []string{IdProperty}
+	return append(all, AllStashedProperties()...)
 }
